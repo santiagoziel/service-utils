87ae4d446ac18a050b4462f2c4c4545c3e72ad9d
santiagoziel <santiago_oziel92@hotmail.com>
2022-12-22 23:38:31 -0600

    cleanup and added comments

--- src/functions.ts
+++ src/functions.ts
@@ -1,11 +1,9 @@
+/**
+ * Creates a new function that calls a given callback with pre-defined arguments.
+ * @param callback the callback function to be called
+ * @param args the arguments to be passed to the callback function
+ * @returns a new function that calls the callback function with the pre-defined arguments
+ */
 export const preload = (callback: Function, ...args: any[]): () => any => {
     return () => callback(...args)
 }
-
-const logString = (a:string) => {
-    console.log(a);
-}
-
-const loaded = preload(logString, "Hi!")
-
-loaded()
--- src/utypes.ts
+++ src/utypes.ts
@@ -1,2 +1,8 @@
+/**
+ * The `Resolution` type represents the result of an operation that can either succeed or fail.
+ * It has two possible states:
+ *   - "succeded": The operation was successful and the result is available in the `value` field.
+ *   - "failed": The operation failed and the reason for the failure is available in the `reason` field.
+ */
 export type Resolution<A> = {state: "succeded", value: A} |
                             {state: "failed", reason: string}
